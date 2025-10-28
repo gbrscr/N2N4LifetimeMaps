7f6f9c06e5d2979d33f9578f6a572b9de6345131
Gabriele Scrivanti <104571095+gbrscr@users.noreply.github.com>
2025-10-28 11:54:03 +0100

    added spe2py

---
+++ codes_and_data/spe2py/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='spe2py',
+    version='1.0.0a',
+    description='This module imports a Princeton Instruments LightField (SPE 3.0) file into a python environment.',
+    author='Alex Hirsch (Zia Lab)',
+    author_email='alexander_hirsch@brown.edu',
+    url='https://github.com/ashirsch/spe2py',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python 3'
+    ],
+    keywords='spectroscopy optics imaging data analysis',
+    py_modules=['spe2py'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'untangle'
+    ]
+)
